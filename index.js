62a7d5e7e1c27bfaf21907885c71acb8e1673439
Leesw_0811 <s2018s15@e-mirim.hs.kr>
2020-03-05 01:55:48 +0900

    #2 Setting Typescri[t UP ✔
    "tsc"

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("hello");
+// powershell에 tsc를 치면 ts파일을 index.js.map파일로 만들어준다.
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,3 @@
+console.log("hello");
+// powershell에 tsc를 치면 ts파일을 index.js.map파일로 만들어준다.
+// node.js는 typescript를 이해하지 못하기 때문에 일반적인 javascript 코드로 컴파일 하는 작업이 필요하다.
