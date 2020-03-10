324904ff6be19fb86e2f370f469849c909ba4970
Leesw_0811 <s2018s15@e-mirim.hs.kr>
2020-03-11 07:50:54 +0900

    #9 Creating a Block part Three ✔
    ERROR
    yarn run v1.22.0
    $ tsc-watch --onSuccess " node dist/index.js"
    Cannot find module 'typescript/bin/tsc
    
    typescript가 삭제되었었음 왜인진 모르겠음 다시 깔아줬더니 에러 해결 🔨

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const CryptoJS = require("crypto-js");
 class Block {
     constructor(index, hash, previousHash, data, timetamp) {
         this.index = index;
@@ -9,7 +10,20 @@ class Block {
         this.timetamp = timetamp;
     }
 }
+Block.calculateBlockHash = (index, previousHash, timetamp, data) => CryptoJS.SHA256(index + previousHash + timetamp + data).toString();
 const genesisBlock = new Block(0, "2020202022020", "", "Hello", 123456);
 let blockchain = [genesisBlock]; //block만 추가 되도록
-console.log(blockchain);
+const getBlockchain = () => blockchain;
+const getLatestBlock = () => blockchain[blockchain.length - 1];
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previousBlock = getLatestBlock();
+    const newIndex = previousBlock.index + 1;
+    const newTimestamp = getNewTimeStamp();
+    const newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    return newBlock;
+};
+console.log(createNewBlock("hello"));
+console.log(createNewBlock("bye bye"));
 //# sourceMappingURL=index.js.map
