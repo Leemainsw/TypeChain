89bca9408e0ad3347fc9b707dc0b00bb9636a0b6
Leesw_0811 <s2018s15@e-mirim.hs.kr>
2020-03-11 07:10:13 +0900

    #7 Blockchain Creating a Block ✔
    ◽◽◽

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,15 @@
 "use strict";
-// console.log("hello");
-// powershell에 tsc를 치면 ts파일을 index.js.map파일로 만들어준다.
-// node.js는 typescript를 이해하지 못하기 때문에 일반적인 javascript 코드로 컴파일 하는 작업이 필요하다.
 Object.defineProperty(exports, "__esModule", { value: true });
-// const name = "yeseul",
-// age = 19,
-// gender = "female";
-class Human {
-    //생성자
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    constructor(index, hash, previousHash, data, timetamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timetamp = timetamp;
     }
 }
-;
-const lynn = new Human('Lynn', 18, "gemale");
-const sayHI = (person) => {
-    return `hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
-    //console.log(`hello ${name}, you are ${age}, you are a ${gender}`);
-};
-console.log(sayHI(lynn));
+const genesisBlock = new Block(0, "2020202022020", "", "Hello", 123456);
+let blockchain = [genesisBlock]; //block만 추가 되도록
+console.log(blockchain);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,28 @@
-// console.log("hello");
-// powershell에 tsc를 치면 ts파일을 index.js.map파일로 만들어준다.
-// node.js는 typescript를 이해하지 못하기 때문에 일반적인 javascript 코드로 컴파일 하는 작업이 필요하다.
+class Block{
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timetamp: number;
 
-// const name = "yeseul",
-// age = 19,
-// gender = "female";
-
-class Human{
-    public name: string;
-    public age: number;
-    public gender: string;
-    //생성자
-    constructor(name: string, age: number, gender?: string) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timetamp: number
+    ) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timetamp = timetamp;
     }
-};
+}
 
-const lynn = new Human('Lynn', 18, "gemale");
+const genesisBlock:Block = new Block(0, "2020202022020","","Hello",123456);
+let blockchain: [Block] = [genesisBlock]; //block만 추가 되도록
 
-const sayHI = (person: Human):string =>{
-    return `hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
-    //console.log(`hello ${name}, you are ${age}, you are a ${gender}`);
-} 
+console.log(blockchain);
 
-console.log(sayHI(lynn));
-//만약 gender를 매개변수로 보내지 않는다면 js에서는 그냥 실행되는데 ts에서는 에러남
-// sayHI = (name,age,gender?) -> 에러 안나고 you are a undefined로 나옴
 export {}; 
