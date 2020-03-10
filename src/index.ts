5c76b3dbc3d7c3ca25840cd8524ab27045dba58f
Leesw_0811 <s2018s15@e-mirim.hs.kr>
2020-03-11 07:23:30 +0900

    #8 Creating a Block part Two ✔
    crypto-js 잘못쳐서 오류날뻔 ;; 후 ;;
    블록체인 강의 듣고 오기 🔨

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
+import * as CryptoJS from "crypto-js";
+
 class Block{
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timetamp: number;
+
+    static calculateBlockHash = (
+        index:number, 
+        previousHash:string, 
+        timetamp:number, 
+        data: string) :string => 
+        CryptoJS.SHA256(index + previousHash + timetamp + data).toString();
 
     constructor(
         index: number,
@@ -21,7 +30,13 @@ class Block{
 }
 
 const genesisBlock:Block = new Block(0, "2020202022020","","Hello",123456);
-let blockchain: [Block] = [genesisBlock]; //block만 추가 되도록
+let blockchain: Block[] = [genesisBlock]; //block만 추가 되도록
+
+const getBlockchain = () : Block[] => blockchain;
+
+const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
 
 console.log(blockchain);
 
